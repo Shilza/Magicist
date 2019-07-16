0871d1143be1ba858a30632772ea5508b17baaee
Shilza <andgalatinov@gmail.com>
2019-07-16 21:18:48 +0300

    Fix Store & add watch/watchProp

--- src/lib/entities/Store.ts
+++ src/lib/entities/Store.ts
@@ -1,36 +1,64 @@
-import {uuidv4} from "../utils/uuidv4";
 import {PubSub} from "../utils/PubSub";
 import '../polyfills/watch';
 
 export class Store {
-    public id: string;
-    public observedState: {
+    private readonly model: {
         watch: (key: string, handler: watchHandler) => void;
     };
+    private static readonly WATCHERS = Symbol;
 
     /**
-     * @param observedState {Object}
+     * @param model {Object}
      * @param middlewares {Array}
      */
-    constructor(observedState, middlewares: Array<middleware> = []) {
-        this.id = uuidv4();
-        this.observedState = observedState;
+    constructor(model, middlewares: Array<middleware> = []) {
+        this.model = model;
         this.registerMiddlewares(middlewares);
         this.registerWatchers();
     }
 
     /**
-     * Method that makes observedState's props observable
+     * @description Return store's model
+     */
+    getModel() {
+        return this.model;
+    }
+
+    /**
+     * @description Method that apply callback when model is changed
+     * @param watcher {Function}
+     */
+    watch(watcher: Function) {
+        PubSub.subscribe(this, () => watcher(this.model));
+    }
+
+    /**
+     * @description Method that apply callback when prop value is changed
+     * @param propName {string}
+     * @param watcher {Function}
+     */
+    watchProp(propName: string, watcher: (oldValue: any, newValue: any) => void) {
+        PubSub.subscribe(Store.WATCHERS, ([prop, oldValue, newValue]) => {
+            if(propName === prop)
+                watcher(oldValue, newValue);
+        });
+    }
+
+    /**
+     * @description Method that makes model's props observable
      */
     private registerWatchers() {
         const watchHandler = (propName, oldValue, newValue) => {
-            if (newValue !== oldValue)
-                PubSub.publish(this.id, propName);
+            if (newValue !== oldValue) {
+                PubSub.publish(this, propName);
+                PubSub.publish(Store.WATCHERS, [propName, oldValue, newValue]);
+            }
             return newValue;
         };
 
-        Object.keys(this.observedState).forEach(key => {
-            this.observedState.watch(key, watchHandler);
+        Object.keys(this.model).forEach(key => {
+            if(typeof key !== 'function')
+                this.model.watch(key, watchHandler);
         });
     }
 
@@ -39,15 +67,15 @@ export class Store {
      * @param middlewares {Array}
      */
     private registerMiddlewares(middlewares: Array<middleware> = []) {
-        Object.entries(this.observedState).forEach(([key, value]) => {
+        Object.entries(this.model).forEach(([key, value]) => {
             if (typeof value === 'function') {
                 const functionsHandler: {} = {
                     apply: (func, args) => {
-                        middlewares.forEach((middleware: any) => middleware(this.observedState, func, args));
+                        middlewares.forEach((middleware: middleware) => middleware(this.model, func, args));
                         func(args);
                     }
                 };
-                this.observedState[key] = new Proxy(value.bind(this.observedState), functionsHandler);
+                this.model[key] = new Proxy(value.bind(this.model), functionsHandler);
             }
         });
     }
