9155ce66533e0e175b98b8c76defbf151b6e0a5a
Shilza <andgalatinov@gmail.com>
2019-07-16 21:19:14 +0300

    Fix codestyle of watch polyfill

--- src/lib/polyfills/watch.js
+++ src/lib/polyfills/watch.js
@@ -2,28 +2,25 @@
 if (!Object.prototype.watch) {
     // eslint-disable-next-line no-extend-native
     Object.defineProperty(Object.prototype, "watch", {
-        enumerable: false
-        , configurable: true
-        , writable: false
-        , value: function (prop, handler) {
-            var
-                oldval = this[prop]
-                , newval = oldval
-                , getter = function () {
+        enumerable: false,
+        configurable: true,
+        writable: false,
+        value: function (prop, handler) {
+            var oldval = this[prop],
+                newval = oldval,
+                getter = function () {
                     return newval;
-                }
-                , setter = function (val) {
+                }, setter = function (val) {
                     oldval = newval;
                     return newval = handler.call(this, prop, oldval, val);
-                }
-            ;
+                };
 
             if (delete this[prop]) { // can't watch constants
                 Object.defineProperty(this, prop, {
-                    get: getter
-                    , set: setter
-                    , enumerable: true
-                    , configurable: true
+                    get: getter,
+                    set: setter,
+                    enumerable: true,
+                    configurable: true
                 });
             }
         }
@@ -34,10 +31,10 @@ if (!Object.prototype.watch) {
 if (!Object.prototype.unwatch) {
     // eslint-disable-next-line no-extend-native
     Object.defineProperty(Object.prototype, "unwatch", {
-        enumerable: false
-        , configurable: true
-        , writable: false
-        , value: function (prop) {
+        enumerable: false,
+        configurable: true,
+        writable: false,
+        value: function (prop) {
             var val = this[prop];
             delete this[prop]; // remove accessors
             this[prop] = val;
