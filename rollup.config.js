c3a5c4f5cbf7dde4825bd03cd7d9fe1b3e913651
Shilza <andgalatinov@gmail.com>
2019-07-17 12:02:53 +0300

    Add minify terser & gzip plugins to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,29 +2,55 @@ import babel from 'rollup-plugin-babel';
 import minify from 'rollup-plugin-babel-minify';
 import pkg from './package.json';
 import resolve from 'rollup-plugin-node-resolve';
+import gzipPlugin from 'rollup-plugin-gzip';
+import {terser} from "rollup-plugin-terser";
 
-export default {
-    input: 'src/lib/index.ts',
-    output: [
-        { file: pkg.module, format: 'es'}
-    ],
-    plugins: [
-        resolve({
-            browser: true,
-            extensions: ['.js', '.jsx', '.ts', '.tsx', '.css'],
-            modulesOnly: true
-        }),
-        babel({
-            presets: ["@babel/typescript"],
-            plugins: [
-                "@babel/plugin-proposal-class-properties",
-            ],
-            exclude: ['node_modules/**'],
-            extensions: ['.js', '.jsx', '.ts', '.tsx'],
-            runtimeHelpers: true
-        }),
-        minify({
-            mangle: false
-        })
-    ]
-};
+const input = 'src/lib/index.ts';
+
+const basicPlugins = [
+    resolve({
+        browser: true,
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.css'],
+        modulesOnly: true
+    }),
+    babel({
+        presets: ["@babel/typescript"],
+        plugins: [
+            "@babel/plugin-proposal-class-properties",
+        ],
+        exclude: ['node_modules/**'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        runtimeHelpers: true
+    })
+];
+
+const minPlugins = [
+    ...basicPlugins,
+    minify(),
+    terser(),
+    gzipPlugin()
+];
+
+export default [
+    {
+        input: input,
+        output: [
+            {file: pkg.main, format: 'es'}
+        ],
+        plugins: basicPlugins
+    },
+    {
+        input: input,
+        output: [
+            {file: pkg.module, format: 'es'}
+        ],
+        plugins: minPlugins
+    },
+    {
+        input: input,
+        output: [
+            {file: pkg.unpkg, format: 'umd', name: 'magicist.umd'}
+        ],
+        plugins: minPlugins
+    }
+];
