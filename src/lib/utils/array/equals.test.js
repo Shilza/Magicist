7954ab1d3ee6c7ba21d3be0c8a0b39990229731e
Shilza <andgalatinov@gmail.com>
2019-07-21 18:35:36 +0300

    Add equals array util

---
+++ src/lib/utils/array/equals.test.js
@@ -0,0 +1,35 @@
+import {equals} from "./equals";
+
+describe('equals', () => {
+    it('should throw TypeError (first param is not array)', () => {
+        expect(() => equals('a', [])).toThrow(TypeError);
+    });
+
+    it('should throw TypeError (second param is not array)', () => {
+        expect(() => equals([], '')).toThrow(TypeError);
+    });
+
+    it('should return false' ,() => {
+        expect(equals([1,2], [2,1])).toBeFalsy();
+    });
+
+    it('should return false(objects comparison)' ,() => {
+        expect(equals([1,{x: 20}], [1, {x: 20}])).toBeFalsy();
+    });
+
+    it('should return false(length of the arrays is not the same)' ,() => {
+        expect(equals([1, 2], [1, 2, 3])).toBeFalsy();
+    });
+
+    it('should return false(nested arrays is not the same)' ,() => {
+        expect(equals([1, [1, 2]], [1, [2, 3]])).toBeFalsy();
+    });
+
+    it('should return true(nested arrays is the same)' ,() => {
+        expect(equals([1, [1, 2]], [1, [1, 2]])).toBeTruthy();
+    });
+
+    it('should return true' ,() => {
+        expect(equals([1, 2], [1, 2])).toBeTruthy();
+    });
+});
---
+++ src/lib/utils/array/equals.ts
@@ -0,0 +1,29 @@
+/**
+ * Function that checks arrays for equality
+ * @param firstArray {Array}
+ * @param secondArray {Array}
+ */
+export function equals (firstArray: Array<any>, secondArray: Array<any>) {
+    if (!Array.isArray(firstArray))
+        throw TypeError('first param must be an array');
+    if (!Array.isArray(secondArray))
+        throw TypeError('second param must be an array');
+
+    // compare lengths - can save a lot of time
+    if (firstArray.length !== secondArray.length)
+        return false;
+
+    for (let i = 0, l = secondArray.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (secondArray[i] instanceof Array && firstArray[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!equals(firstArray[i], secondArray[i]))
+                return false;
+        }
+        else if (secondArray[i] !== firstArray[i]) {
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;
+        }
+    }
+    return true;
+}
